ebceba085cea043bf0e7aafbfaab06ae296cd13d
Simon Wrang <simon.wrang2@gmail.com>
2018-10-27 19:02:11 +0200

    Updates

---
+++ precache-manifest.a29138a9b7cba4606b38100c8be4f9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloworld/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "607eba3de01519428994",
+    "url": "/helloworld/static/js/runtime~main.607eba3d.js"
+  },
+  {
+    "revision": "2c7d8becb8e6ca92d076",
+    "url": "/helloworld/static/js/main.2c7d8bec.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/helloworld/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "2c7d8becb8e6ca92d076",
+    "url": "/helloworld/static/css/main.841caac1.chunk.css"
+  },
+  {
+    "revision": "eebadb2efc58a02d350c979632eaba4e",
+    "url": "/helloworld/index.html"
+  }
+];
